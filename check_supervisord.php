90161c82823657802c64542615f0e9095c72d6ce
Tim Groeneveld <tim@timg.ws>
2014-05-27 11:57:05 +1000

    Small code style changes to check_supervisord

--- check_supervisord.php
+++ check_supervisord.php
@@ -11,31 +11,35 @@ class SuperVisorNagios
     const NAGIOS_STATE_CRITICAL  = 2;
     const NAGIOS_STATE_UNKNOWN   = 3;
     const NAGIOS_STATE_DEPENDENT = 4;
-    
+
     /** @see http://supervisord.org/subprocess.html#process-states 
-     * STOPPED (0)
-       The process has been stopped due to a stop request or has never been started.
-       
-       STARTING (10)
-       The process is starting due to a start request.
+        
+     STOPPED (0)
+     The process has been stopped due to a stop request or has never been started.
 
-       RUNNING (20)
-       The process is running.
+     STARTING (10)
+     The process is starting due to a start request.
 
-       BACKOFF (30)
-       The process entered the STARTING state but subsequently exited too quickly to move to the RUNNING state.
-       
-       STOPPING (40)
-       The process is stopping due to a stop request.
-       
-       EXITED (100)
-       The process exited from the RUNNING state (expectedly or unexpectedly).
-       
-       FATAL (200)
-       The process could not be started successfully.
-       
-       UNKNOWN (1000)
-       The process is in an unknown state (supervisord programming error). */
+     RUNNING (20)
+     The process is running.
+
+     BACKOFF (30)
+     The process entered the STARTING state but subsequently exited too quickly to move to the RUNNING state.
+
+     STOPPING (40)
+     The process is stopping due to a stop request.
+
+     EXITED (100)
+     The process exited from the RUNNING state (expectedly or unexpectedly).
+
+     FATAL (200)
+     The process could not be started successfully.
+
+     UNKNOWN (1000)
+     The process is in an unknown state (supervisord programming error). 
+
+     */
+
     const SUPERVISOR_STATE_STOPPED  = 0;
     const SUPERVISOR_STATE_STARTING = 10;
     const SUPERVISOR_STATE_RUNNING  = 20;
@@ -44,26 +48,28 @@ class SuperVisorNagios
     const SUPERVISOR_STATE_EXITED   = 100;
     const SUPERVISOR_STATE_FATAL    = 200;
     const SUPERVISOR_STATE_UNKNOWN  = 1000;
-    
+
     const SUPERVISOR_RPC_REQUEST_LISTPROCESS = 'supervisor.getAllProcessInfo';
-    
+
     private $streamContext;
     private $serverName;
     private $grepPattern;
-    
+
     public function __construct()
     {
-        $this->streamContext = stream_context_create(array('http' => array(
-        'method' => "POST",
-        'header' => "Content-Type: text/xml",
-        )));
+        $this->streamContext = stream_context_create(array(
+            'http' => array(
+                'method' => 'POST',
+                'header' => 'Content-Type: text/xml',
+            )
+        ));
 
-        if(!extension_loaded("xmlrpc")) {
-            $this->writeError("PHP extension ext/xmlrpc is needed but not found", self::NAGIOS_STATE_UNKNOWN);
+        if(!extension_loaded('xmlrpc')) {
+            $this->writeError('PHP extension ext/xmlrpc is needed but not found', self::NAGIOS_STATE_UNKNOWN);
         }
-        
-        if($_SERVER['argc'] != 5 || (($getopt = getopt("h:p:")) === false)) {
-            $this->writeError("Wrong script usage : use -h <hostname> -p <pattern>", self::NAGIOS_STATE_UNKNOWN);
+
+        if($_SERVER['argc'] != 5 || (($getopt = getopt('h:p:')) === false)) {
+            $this->writeError('Wrong script usage : use -h <hostname> -p <pattern>', self::NAGIOS_STATE_UNKNOWN);
         }
         $this->serverName  = $getopt['h'];
         $this->grepPattern = preg_quote($getopt['p'], '/');    
@@ -71,28 +77,30 @@ class SuperVisorNagios
 
     private function xmlRPCRequest($request, array $params = array())
     {
-        stream_context_set_option($this->streamContext, "http", "content", xmlrpc_encode_request($request, $params));
-        $response = @file_get_contents("http://$this->serverName:9001/RPC2", false, $this->streamContext);
-        
+        stream_context_set_option($this->streamContext, 'http', 'content', xmlrpc_encode_request($request, $params));
+        $response = @file_get_contents('http://' . $this->serverName . ':9001/RPC2', false, $this->streamContext);
+
         if($response === false) {
             $lastError = error_get_last();
-            $this->writeError("Could not contact server $this->serverName : {$lastError['message']}", self::NAGIOS_STATE_UNKNOWN);
+            $this->writeError("Could not contact server {$this->serverName} : {$lastError['message']}", self::NAGIOS_STATE_UNKNOWN);
         }
+
         $response = xmlrpc_decode($response);
         if ($response && xmlrpc_is_fault($response)) {
             $this->writeError("Could not decode XML-RPC : {$response['faultString']}", self::NAGIOS_STATE_UNKNOWN);
         }
+
         return $response;
     }
-    
+
     public function process()
     {
         $superVisorProcessList = $this->xmlRPCRequest(self::SUPERVISOR_RPC_REQUEST_LISTPROCESS);
         $deadProcesses = array();
 
         foreach ($superVisorProcessList as $process) {
-            if (isset($process['name']) && preg_match("/$this->grepPattern/",$process['name'])) {
-        
+            if (isset($process['name']) && preg_match('/' . $this->grepPattern . '/',$process['name'])) {
+
                 if($process['state'] != self::SUPERVISOR_STATE_RUNNING) {
                     $deadProcesses[$process['name']] = $process['spawnerr'];
                 }
@@ -106,17 +114,17 @@ class SuperVisorNagios
 
         $this->exitOk();
     }
-    
+
     private function writeError($errorMsg, $exitStatus)
     {
-        echo str_replace("\n", "", $errorMsg) . "\n";
+        echo str_replace("\n", '', $errorMsg) . "\n";
         exit($exitStatus);
     }
-    
+
     private function exitOk()
     {
-        echo "All is right \n";
-	exit(self::NAGIOS_STATE_OK);
+        echo "All is right\n";
+        exit(self::NAGIOS_STATE_OK);
     }
 }
 
